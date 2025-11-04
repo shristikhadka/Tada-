865fe196cd1b40d9865053315096456c9a9fc575
Shristi Khadka <shristikhadka0988@gmail.com>
2025-11-03 21:37:02 -0500

    Add public endpoints for user registration and health check

---
+++ src/main/java/com/shristikhadka/demo/controller/PublicController.java
@@ -0,0 +1,33 @@
+package com.shristikhadka.demo.controller;
+
+import com.shristikhadka.demo.entity.User;
+import com.shristikhadka.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/public")
+public class PublicController
+{
+
+    private final UserService userService;
+
+    public PublicController(UserService userService){
+        this.userService=userService;
+    }
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "ok";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User>registerUser(@RequestBody User user){
+        log.info("creating new user: {}",user.getUserName());
+        User createdUser=userService.saveNewUser(user);
+        return ResponseEntity.status(201).body(createdUser);
+    }
+}
