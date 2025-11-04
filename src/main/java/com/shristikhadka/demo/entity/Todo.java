b9b3b68d8f2aa935263460387d674a5990f2691e
Shristi Khadka <shristikhadka0988@gmail.com>
2025-11-03 21:35:18 -0500

    Add User and Todo entities with JPA annotations

---
+++ src/main/java/com/shristikhadka/demo/entity/Todo.java
@@ -0,0 +1,26 @@
+package com.shristikhadka.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private boolean completed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user;
+}
---
+++ src/main/java/com/shristikhadka/demo/entity/User.java
@@ -0,0 +1,30 @@
+package com.shristikhadka.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private  String userName;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private @NonNull String password;
+    @ElementCollection
+    private List<String> roles;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Todo> todoList = new ArrayList<>();
+}
